918053d2b077abfca80440fd443c5ae27c5a064a
Vytautas Gimbutas <vytautas@gimbutas.net>
2019-05-29 14:29:01 +0300

    toIban can be optional

--- PayseraMokejimaiSDK/Entities/Transfer/PSManualTransferConfiguration.swift
+++ PayseraMokejimaiSDK/Entities/Transfer/PSManualTransferConfiguration.swift
@@ -5,12 +5,12 @@ public class PSManualTransferConfiguration: Mappable {
     public let toBankKey: String
     public let currency: String
     public let toCountryCode: String
-    public let toIban: String
     public let toBeneficiaryName: String
     public let commission: PSMoney
     public let transferExecutionTime: String
     
     public var fromCountryCode: String?
+    public var toIban: String?
     public var toAccountNumber: String?
     public var toBankCode: String?
     public var toBic: String?
@@ -31,7 +31,6 @@ public class PSManualTransferConfiguration: Mappable {
             toBankKey = try map.value("to_bank_key")
             currency = try map.value("currency")
             toCountryCode = try map.value("to_country_code")
-            toIban = try map.value("to_iban")
             toBeneficiaryName = try map.value("to_beneficiary_name")
             commission = try map.value("commission")
             transferExecutionTime = try map.value("transfer_execution_time")
@@ -42,6 +41,7 @@ public class PSManualTransferConfiguration: Mappable {
     
     public func mapping(map: Map) {
         fromCountryCode             <- map["from_country_code"]
+        toIban                      <- map["to_iban"]
         toAccountNumber             <- map["to_account_number"]
         toBankCode                  <- map["to_bank_code"]
         toBic                       <- map["to_bic"]
