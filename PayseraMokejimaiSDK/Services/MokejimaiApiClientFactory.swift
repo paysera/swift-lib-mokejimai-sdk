0339744649662bd07ad02b95317c5bfc874eb50b
Vytautas Gimbutas <vytautas@gimbutas.net>
2019-06-19 17:19:46 +0300

    Factory method extended with logger parameter

--- PayseraMokejimaiSDK/Services/MokejimaiApiClientFactory.swift
+++ PayseraMokejimaiSDK/Services/MokejimaiApiClientFactory.swift
@@ -1,15 +1,22 @@
 import Foundation
 import Alamofire
+import PayseraCommonSDK
 
 public class MokejimaiApiClientFactory {
     public static func createTransferApiClient(
         headers: MokejimaiRequestHeaders,
         credentials: MokejimaiApiCredentials,
-        tokenRefresher: TokenRefresherProtocol? = nil
+        tokenRefresher: TokenRefresherProtocol? = nil,
+        logger: PSLoggerProtocol? = nil
     ) -> MokejimaiApiClient {
         let sessionManager = SessionManager()
         sessionManager.adapter = MokejimaiRequestAdapter(credentials: credentials, headers: headers)
         
-        return MokejimaiApiClient(sessionManager: sessionManager, credentials: credentials, tokenRefresher: tokenRefresher)
+        return MokejimaiApiClient(
+            sessionManager: sessionManager,
+            credentials: credentials,
+            tokenRefresher: tokenRefresher,
+            logger: logger
+        )
     }
 }
