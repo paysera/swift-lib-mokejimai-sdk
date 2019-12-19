b60a85e773cd8a0ab329c6b7392c2ec446edad8f
Rokas Ambrazevicius <r.ambrazevicius@paysera.lt>
2019-12-19 09:31:20 +0200

    Edit test

--- PayseraMokejimaiSDKTests/PayseraMokejimaiSDKTests.swift
+++ PayseraMokejimaiSDKTests/PayseraMokejimaiSDKTests.swift
@@ -6,12 +6,12 @@ import PromiseKit
 @testable import PayseraMokejimaiSDK
 
 class PayseraMokejimaiSDKTests: XCTestCase {
-    private let jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJtb2tlamltYWkiLCJpc3MiOiJhdXRoX2FwaSIsImV4cCI6MTU3NjYyODgzOCwianRpIjoieUYwUFd4cHZ0QXh4cE5mSHNmMTVGcmFiS1Q5WVRNZDEiLCJwc3I6cyI6WyJsb2dnZWRfaW4iLCJjb25maXJtZWRfbG9nX2luIl0sInBzcjp1IjoiMTAxMjAwNyIsInBzcjpzaWQiOiJLb0d4OWh0d1YzdVFUVUVad3FlWDhBWkp5WDRGU3dRQiIsInBzcjphIjp7InVzZXJfaWQiOiIxMDEyMDA3In0sImlhdCI6MTU3NjU4NTYzOH0.ATogdC6ikHaOnZ02S9fPJVSCsdZZidNab4ItcUkglcHGtY3WhY9qszI66Og4OeNvdZv_eBYWZO0FBEH98pjoGQS9tTm1wLVvzQQChs3VlyKpq506W1lKxbtdUgAzCqn8QBrGSudE5KnJdQ7tpyzo8rvvHas7BZPBTktnGUS164JSm4kgA4B-2KPt_t_QxqXnjczMgNynHZoWCQig6KENoAkDBJnkhWYmVy7x7dk1F6MMag4XkgZwbD3zg-SSviBnyXfrSF9M3OQuwSx5fEBKki13fHQk0GvEbBIOe-_p_k0ErGkKlfvCL8tUflzvCWNVGNSWL9qVzqMn-4Lh7dvRZApxn1r0Rz7K0FQTDzXyWsAMm6CAmLTY1sfbXSag-phN_1KgItyzLgTzpn3EelDYX54piQSnTGtoIu9srOtOMfFacFLLB_EMjKxVO8sPwAXJ-s5QYEwj6q3GfeXvEnUZQx4XUtYsK7C4HnWaysdGFiWso2LlfB7GZs8py9O2N6W1kl8ySuiI_PkI4IDCJRJ-G-qXqr90Mzd4pnqBxEYupP0JzNp9UiXTS_PEDy68lZc42dVapzMpvRGgPTaeNwYq-A2HgULf7hx_7oaWM6zAUQU9BGPcvQGHfLEItqM8CXVTGsJ3WPb2c59P_q9MKo1WTGAtx6SLcphRtXPeApq7pY4"
+    private let jwtToken = "insert_me"
     private let language = "en"
 
     func testSendLog() {
-        let userId = "1012007"
-        let appVersion = "6.0.0"
+        let userId = "insert_me"
+        let appVersion = "insert_me"
         
         let action = "app_unlocked"
         let context = ["app_version": appVersion]
