b4874e03ba48823a2ac05abc37890141891731c0
Milda Mauricaite <m.mauricaite@paysera.lt>
2020-05-04 09:51:36 +0300

    add id

--- PayseraMokejimaiSDK/Entities/User/PSUserAccountData.swift
+++ PayseraMokejimaiSDK/Entities/User/PSUserAccountData.swift
@@ -1,6 +1,7 @@
 import ObjectMapper
 
 public class PSUserAccountData: Mappable {
+    public var id: Int!
     public var code: String?
     public var displayName: String!
     public var type: String!
@@ -11,6 +12,7 @@ public class PSUserAccountData: Mappable {
     }
     
     public func mapping(map: Map) {
+        id                  <- map["id"]
         code                <- map["code"]
         displayName         <- map["display_name"]
         type                <- map["type"]
