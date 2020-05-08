ebb3b75a5b566f8ce8be74ccdd6bde73e47f25d3
Milda Mauricaite <m.mauricaite@paysera.lt>
2020-05-08 09:18:00 +0300

    add identifier to PSUserAccountData

--- PayseraMokejimaiSDK/Entities/User/PSUserAccountData.swift
+++ PayseraMokejimaiSDK/Entities/User/PSUserAccountData.swift
@@ -2,6 +2,7 @@ import ObjectMapper
 
 public class PSUserAccountData: Mappable {
     public var id: Int!
+    public var identifier: String!
     public var code: String?
     public var displayName: String?
     public var type: String!
@@ -13,6 +14,7 @@ public class PSUserAccountData: Mappable {
     
     public func mapping(map: Map) {
         id                  <- map["id"]
+        identifier          <- map["identifier"]
         code                <- map["code"]
         displayName         <- map["display_name"]
         type                <- map["type"]
