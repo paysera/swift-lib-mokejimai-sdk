4ae697804a6bd3bd4297f1508ec34678128d6aca
Pattio <Pattio@users.noreply.github.com>
2020-05-20 10:51:33 +0300

    Use PSTrustedSession (#9)

--- PayseraMokejimaiSDK/Services/MokejimaiApiClientFactory.swift
+++ PayseraMokejimaiSDK/Services/MokejimaiApiClientFactory.swift
@@ -9,10 +9,14 @@ public class MokejimaiApiClientFactory {
         tokenRefresher: PSTokenRefresherProtocol? = nil,
         logger: PSLoggerProtocol? = nil
     ) -> MokejimaiApiClient {
-        let session = Session(interceptor: PSRequestAdapter(credentials: credentials, headers: headers))
+        let interceptor = PSRequestAdapter(credentials: credentials, headers: headers)
+        let trustedSession = PSTrustedSession(
+            interceptor: interceptor,
+            hosts: ["bank.paysera.com"]
+        )
         
         return MokejimaiApiClient(
-            session: session,
+            session: trustedSession,
             credentials: credentials,
             tokenRefresher: tokenRefresher,
             logger: logger
