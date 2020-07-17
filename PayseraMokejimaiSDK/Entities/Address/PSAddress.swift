092e6ff645aded4b11177aa4d685e46d415762b7
Vytautas Gimbutas <v.gimbutas@paysera.com>
2020-07-17 13:35:57 +0300

    Added legacyCityName to PSAddress

--- PayseraMokejimaiSDK/Entities/Address/PSAddress.swift
+++ PayseraMokejimaiSDK/Entities/Address/PSAddress.swift
@@ -8,6 +8,7 @@ public class PSAddress: Mappable {
     public var transliteratedCityName: String!
     public var postalCode: String!
     public var legacyAddress: String!
+    public var legacyCityName: String?
     public var streetName: String!
     public var houseNumber: String!
     public var apartmentNumber: String!
@@ -25,6 +26,7 @@ public class PSAddress: Mappable {
         transliteratedCityName <- map["transliterated_city_name"]
         postalCode <- map["postal_code"]
         legacyAddress <- map["legacy_address"]
+        legacyCityName <- map["legacy_city_name"]
         streetName <- map["street_name"]
         houseNumber <- map["house_number"]
         apartmentNumber <- map["apartment_number"]
