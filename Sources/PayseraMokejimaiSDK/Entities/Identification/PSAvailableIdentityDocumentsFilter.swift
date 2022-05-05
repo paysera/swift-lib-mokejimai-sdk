2166d033cc74f2d56e58110396fdd832c92386e9
Aram Semerjyan <aramsemerjyan@Arams-MacBook-Pro.local>
2022-05-05 12:00:01 +0400

    Pass user ID to illustration filter

--- Sources/PayseraMokejimaiSDK/Entities/Identification/PSAvailableIdentityDocumentsFilter.swift
+++ Sources/PayseraMokejimaiSDK/Entities/Identification/PSAvailableIdentityDocumentsFilter.swift
@@ -3,9 +3,11 @@ import PayseraCommonSDK
 
 public class PSAvailableIdentityDocumentsFilter: PSBaseFilter {
     public var country: String?
+    public var userId: String?
     
     public override func mapping(map: Map) {
         super.mapping(map: map)
         country       <- map["country"]
+        userId        <- map["user_id"]
     }
 }
